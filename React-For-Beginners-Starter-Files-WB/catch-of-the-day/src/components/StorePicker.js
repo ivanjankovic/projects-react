384007088c78a9dc3e336520493ebaaf5a4457dc
ivan Jankovic <jankoemail@gmail.com>
2019-03-05 18:45:25 -0500

    basics

---
+++ React-For-Beginners-Starter-Files-WB/catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { format } from 'path';
+
+class StorePicker extends React.Component {
+  render() {
+    return <form className="store-selector">
+      <h2>Please Enter A Store</h2>
+    </form>;
+  }
+}
+
+export default StorePicker;
